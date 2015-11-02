725014e6c80080dce23a460a5b68a5c017964ac1
Kiffin Gish <kiffin.gish@planet.nl>
2015-11-02 22:02:51 +0100

    Redirect users to category index if category id invalid

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,7 @@
 class CategoriesController < ApplicationController
+
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
   def index
     @categories = Category.all
   end
@@ -20,15 +23,12 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
     @category.update(category_params)
 
     if @category.update(category_params)
@@ -41,7 +41,6 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id])
     @category.destroy
 
     flash[:notice] = "Category has been deleted."
@@ -53,4 +52,12 @@ class CategoriesController < ApplicationController
   def category_params
     params.require(:category).permit(:name, :description)
   end
+
+  def set_category
+    @category = Category.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The category you were looking for could not be found."
+    redirect_to categories_path
+  end
+
 end
