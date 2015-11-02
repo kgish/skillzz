1121ce51607da9a44d0f7eb79821144d05a8e808
Kiffin Gish <kiffin.gish@planet.nl>
2015-11-02 17:25:39 +0100

    Add the ability to view all categories

---
+++ spec/factories/category_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :category do
+    name "Programming"
+    description "Anything to do with software development"
+  end
+end
---
+++ spec/features/viewing_categories_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.feature "Users can view categories" do
+  scenario "with the category details" do
+    category = FactoryGirl.create(:category, name: "Programming")
+    visit categories_url
+    click_link "Programming"
+    expect(page.current_url).to eq category_url(category)
+  end
+end
