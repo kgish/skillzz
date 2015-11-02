fc90a3eef916eacd9d1614372f0f0ec79057d958
Kiffin Gish <kiffin.gish@planet.nl>
2015-11-02 20:58:39 +0100

    Categories can now be updated

---
+++ spec/features/editing_categories_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.feature "Users can edit existing categories" do
+  before do
+    FactoryGirl.create(:category, name: "Household")
+
+    visit categories_url
+    click_link "Household"
+    click_link "Edit Category"
+  end
+
+  scenario "with valid attributes" do
+    fill_in "Name", with: "Household #2"
+    click_button "Update Category"
+
+    expect(page).to have_content "Category has been updated."
+    expect(page).to have_content "Household #2"
+  end
+
+  scenario "when providing invalid attributes" do
+    fill_in "Name", with: ""
+    click_button "Update Category"
+    expect(page).to have_content "Category has not been updated."
+end
+
+end
