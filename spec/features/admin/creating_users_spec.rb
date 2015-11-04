f2d6d4c64625577cf43743d7afb08da9ac483d94
Kiffin Gish <kiffin.gish@planet.nl>
2015-11-04 19:09:09 +0100

    Create admin users as an admin.

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -17,4 +17,14 @@ RSpec.feature "Admins can create new users" do
     click_button "Create User"
     expect(page).to have_content "User has been created."
   end
+
+  scenario "when the new user is an admin" do
+    fill_in "Email", with: "admin2@skillzz.com"
+    fill_in "Password", with: "password"
+    check "Is an admin?"
+    click_button "Create User"
+    expect(page).to have_content "User has been created."
+    expect(page).to have_content "admin2@skillzz.com (Admin)"
+  end
+
 end
