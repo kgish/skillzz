8105aeec3a459f04986a18da2108bd7bcc36353e
Kiffin Gish <kiffin.gish@planet.nl>
2015-11-14 21:34:40 +0100

    User can login using either username or email.

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -3,22 +3,33 @@ require "rails_helper"
 RSpec.feature "Users can sign in" do
   let!(:user) { FactoryGirl.create(:user) }
 
-  scenario "with valid credentials" do
+  scenario "with valid credentials (email)" do
     visit "/"
     click_link "Sign in"
-    fill_in "Email", with: user.email
+    fill_in "Username or email", with: user.email
     fill_in "Password", with: "password"
     click_button "Sign in"
 
     expect(page).to have_content "Signed in successfully."
-    expect(page).to have_content "Signed in as #{user.email}"
+    expect(page).to have_content "Signed in as #{user.username}"
+  end
+
+  scenario "with valid credentials (username)" do
+    visit "/"
+    click_link "Sign in"
+    fill_in "Username or email", with: user.username
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+
+    expect(page).to have_content "Signed in successfully."
+    expect(page).to have_content "Signed in as #{user.username}"
   end
 
   scenario "unless they are archived" do
     user.archive
     visit "/"
     click_link "Sign in"
-    fill_in "Email", with: user.email
+    fill_in "Username or email", with: user.email
     fill_in "Password", with: "password"
     click_button "Sign in"
 
